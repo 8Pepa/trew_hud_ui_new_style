74096547c66c60ed496b1021dad05e3d6e90d270
$Pepa <54434914+8Pepa@users.noreply.github.com>
2020-11-19 20:02:56 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,68 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.serverLogo = ''
+
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= '~',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KMH',
+	maxSpeed = 240,
+
+	keys = {
+		seatbelt 	= 'B',
+		cruiser		= '',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= false,
+
+	showJob		 		= false,
+
+	showWalletMoney 	= false,
+	showBankMoney 		= false,
+	showBlackMoney 		= false,
+	showSocietyMoney	= false,
+
+	showDate 			= false,
+	showLocation 		= true,
+	showVoice	 		= false,
+
+	showHealth			= true,
+	showArmor	 		= true,
+	showStamina	 		= true,
+	showHunger 			= true,
+	showThirst	 		= true,
+
+	showMinimap			= false,
+
+	showWeapons			= false,	
+}
